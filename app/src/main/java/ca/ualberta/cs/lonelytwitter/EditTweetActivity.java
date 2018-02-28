81d73dffca83a23ff14e2f59bbb9ae7d91920420
99sshimizu <99sarah.s@gmail.com>
2018-02-27 18:44:03 -0700

    Initial commit

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/EditTweetActivity.java
@@ -0,0 +1,15 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SarahS on 2018/02/27.
+ */
+
+public class EditTweetActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+    }
+}
